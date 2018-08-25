07f102dbda716f9aaf2f2bc7c4d8a0e1b18063d8
Henrique Barcelos <henriquebarcelos@easycarros.com>
2018-08-25 19:41:21 -0300

    Inlined use cases and macros for tests

--- logger.test.js
+++ logger.test.js
@@ -1,9 +1,32 @@
 const test = require('ava');
-const testMethod = require('./test/macros/test-method');
-const loggerMethodUseCases = require('./test/use-cases/logger-methods');
 const createLogger = require('./logger');
 const parseJSONStream = require('./utils/parse-json-stream');
 const streamToGenerator = require('./utils/stream-to-generator');
+
+const loggerMethodUseCases = [
+  { input: { method: 'trace' }, expected: { level: 10 } },
+  { input: { method: 'debug' }, expected: { level: 20 } },
+  { input: { method: 'info' }, expected: { level: 30 } },
+  { input: { method: 'warn' }, expected: { level: 40 } },
+  { input: { method: 'error' }, expected: { level: 50 } },
+  { input: { method: 'fatal' }, expected: { level: 60 } },
+];
+
+async function testMethod(t, input = {}, expected = {}) {
+  const { method } = input;
+  const { level } = expected;
+
+  const stream = parseJSONStream();
+  const logger = createLogger({ level: 'trace' }, stream);
+
+  const gen = streamToGenerator(stream);
+
+  logger[method]('foo');
+
+  const data = await gen.next().value;
+
+  t.is(data.level, level);
+}
 
 loggerMethodUseCases.forEach(({ input, expected }) => {
   test(
